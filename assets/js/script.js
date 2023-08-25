b0823151ad034ef832d131515c3c37043ef5687e
devmello <kol.kid.py@gmail.com>
2023-08-24 19:00:01 -0700

    rewrote entire website

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,188 +1,125 @@
-// print some info
-console.log('source for this website is on https://github.com/devmello/devmello.cf');
-
-// array prototype extension
-Array.prototype.random = function() {
-    return this[Math.floor((Math.random() * this.length))];
-}
-
-// is mobile
 let mobile = 'ontouchstart' in document.documentElement;
 
-// current page
-let page = 'enter';
+let switchAllowed = false;
 
-// finished typing all text
-let done = false;
+let boxClicks = 0;
 
-// audio spectrum thing
-let wave;
-let shouldRender = false;
-let renderConfirmed = false;
+function openSocial(type) {
+  let url = 'about:blank';
 
-// text index
-let currentlyOn = 0;
+  switch (type) {
+    case 'discord':
+      url = 'https://discord.com/users/';
+      break;
+    case 'github':
+      url = 'https://github.com/devmello';
+      break;
+    case 'instagram':
+      url = 'https://instagram.com/devmello';
+      break;
+    case 'linkedin':
+      url = 'https://linkedin.com/in/devmello';
+      break;
+    case 'resume':
+      url = 'https://devmello.github.io/assets/content/resume.pdf';
+      break;
+  }
 
-// list of audio played (dont play again until loop)
-let songsPlayed = [];
-
-// define messages to display
-let messages = [
-    'hey, thanks for stopping by!',
-    'i\'m devmello.',
-    'i develop html, css, node.js, java, c# & python.',
-    'my projects include:',
-    '<a target="_blank" href="https://actova.ac">actova.ac</a> - the #1 minecraft screenshare tool.',
-    '<a target="_blank" href="https://devmello.cf/Jello">DevJello</a> - the best remake of the original Jello client.',
-    '<a target="_blank" href="https://zarzul.com">zarzul.com</a> - enterprise ddos mitigation solutions.',
-    'you can find my github <a target="_blank" href="https://github.com/devmello">here</a>.',
-    'feel free to contact me via discord @ DevMello#0994.',
-    'you can also send me an email via dev@devmello.cf.'
-];
-
-// define songs
-let songs = [
-    ['controlla.mp3', 'idealism - controlla']
-]
-
-// loaded audio callback
-function loadedAudioCallback() {
-    console.log('audio loaded callback executed');
-    // play audio
-    wave.play();
+  window.open(url);
 }
 
-// ended audio callback
-function endedAudioCallback() {
-    // do some checks
-    if (songsPlayed.length == songs.length) songsPlayed = [];
+function startIntroTyping() {
+  new TypeIt('#intro-text', {
+    speed: 50,
+  })
+    .type('welcome.', { delay: 1200 })
+    .delete(null, { delay: 1000 })
+    .type(`${mobile ? 'tap' : 'press any key'} to enter.`)
+    .go();
 
-    // get a random song
-    let song = songs.filter(s => !songsPlayed.includes(s[0])).random();
-
-    // push to the songsPlayed array
-    songsPlayed.push(song[0]);
-
-    console.log(`selected song: ${song[0]}`);
-
-    // play song
-    wave = new CircularAudioWave(document.getElementById('canvas'));
-
-    // load assets/song.mp3 then play
-    wave.loadAudio(`assets/songs/${song[0]}`);
-
-    // set the currently playing text
-    $('#playingtext').text(song[1]);
-
-    console.log('loading audio');
+  setTimeout(function () {
+    switchAllowed = true;
+  }, 2500);
 }
 
-// on document load
-$(document).ready(function() {
-    if (mobile) {
-        $('#entertext').text('press anywhere to enter');
-    }
+function typerStartTyping(typer) {
+  typer.reset();
 
-    // fade in the underscore once before the interval executes
-    $('#underscore').fadeOut(500).fadeIn(500);
-    setInterval(function() {
-        // fade in and out the underscore
-        $('#underscore').fadeOut(500).fadeIn(500);
-    }, 1000);
+  let text = ['javascript', 'sql', 'java', 'c++', 'python', 'kotlin', 'html', 'css'];
+
+  text.forEach(function (language, index) {
+    typer.move(null);
+    typer.type(language, { delay: 1000 });
+    typer.pause(1000);
+
+    typer.delete(language.length, { delay: 1000 });
+  });
+
+  typer.go();
+}
+
+function startMainTyping() {
+  let typer = new TypeIt('#subtext', {
+    speed: 50,
+    afterComplete: async () => {
+      typerStartTyping(typer);
+    },
+  });
+
+  typerStartTyping(typer);
+}
+
+function switchScreen() {
+  document.title = 'devmello.me | home';
+
+  $('.intro').fadeOut(1000, function () {
+    $('.bg-image').fadeIn(1000);
+    $('.main').fadeIn(1000, function () {
+      startMainTyping();
+    });
+  });
+
+  ['background', 'rain'].forEach(function (audioName) {
+    let fullPath = `assets/audio/${audioName}.mp3`;
+
+    let audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', fullPath);
+    audioElement.style.display = 'none';
+
+    audioElement.addEventListener('ended', function () {
+      this.currentTime = 0;
+      this.play();
+    });
+
+    //audioElement.play();
+  });
+}
+
+document.addEventListener('keydown', function (e) {
+  if (switchAllowed) {
+    switchAllowed = false;
+    switchScreen();
+  }
 });
 
-// load the main page and execute required functions
-function startIt() {
-    // set the document title
-    document.title = 'devmello.cf | home';
-
-    // get a random song
-    let song = songs.random();
-
-    // push to the songsPlayed array
-    songsPlayed.push(song[0]);
-
-    console.log(`selected song: ${song[0]}`);
-
-    // play song
-    wave = new CircularAudioWave(document.getElementById('canvas'));
-
-    // load assets/song.mp3 then play
-    wave.loadAudio(`assets/songs/${song[0]}`);
-
-    // set the currently playing text
-    $('#playingtext').text(song[1]);
-
-    console.log('loading audio');
-
-    // fade intro out
-    $('#intro').fadeOut(500);
-    // fade in info
-    $('#info').fadeIn(500);
-    // fade in bg-image
-    $('#bg-image').fadeIn(500);
-    // change page
-    page = 'info';
-
-    // start indexing the text
-    newText();
-}
-
-// on key press
-$(document).keypress(function(e) {
-    if (page === 'enter' && !mobile) {
-        startIt();
-    }
+document.addEventListener('touchstart', function (e) {
+  if (switchAllowed && mobile) {
+    switchAllowed = false;
+    switchScreen();
+  }
 });
 
-// on tap
-$(document).on('click', function(e) {
-    if (page === 'enter' && mobile) {
-        startIt();
+document.addEventListener('DOMContentLoaded', function () {
+  startIntroTyping();
+  document.onselectstart = () => false;
+  $('.box').click(() => {
+    boxClicks++;
+
+    if (boxClicks === 10) {
+      // fade out box
+      $('.box').fadeOut(1000, () => {
+        document.body.requestFullscreen();
+      });
     }
+  });
 });
-
-// type new text
-function newText() {
-    // insert a new element into the dom
-    $('#contentinner').append(`<div class="instruction"> <span class="user">devmello@127.0.0.1</span>: <span class="path">~</span>$ <span id="command${currentlyOn}" class="command"></span> <span class="blinking-cursor" id="blinkingcursor">_</span> </div>`);
-    // initialize a new instance of typeit
-    new TypeIt(`#command${currentlyOn}`, {
-        strings: messages[currentlyOn],
-        speed: 55,
-        cursor: false,
-        afterComplete: async (instance) => {
-            // increment the current text index
-            currentlyOn++;
-            // remove the blinking cursor
-            document.getElementById('blinkingcursor').remove();
-            // check if we have reached the last message
-            if (currentlyOn == messages.length) {
-                done = true;
-                console.log('no more messages.');
-            } else {
-                console.log(`going to new text, index: ${currentlyOn}`);
-                setTimeout(newText, 500);
-            }
-        }
-    }).go();
-}
-
-// show the audio visualizer
-function visualizeAudio() {
-    // only allow opening the audio visualizer if all text has been displayed
-    if (done) {
-        // if on mobile display a warning
-        if (mobile && !renderConfirmed) {
-            alert('Hey! The audio visualizer is not optimized for mobile. Your device may become unresponsive if you enable this. Click again to confirm.');
-            renderConfirmed = true;
-        } else {
-            // start rendering
-            shouldRender = true;
-            // show the element with the id "canvas"
-            $('#canvas').show();
-            // hide the element with the id "cv-code"
-            $('#cv-code').hide();
-        }
-    }
-}
